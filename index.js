9319a81853ca74087705a7b7a50b74b863f04619
Tracie35 <kariukitracie@gmail.com>
2022-06-07 10:30:56 +0300

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-
-// Write your code here
+let num1=2,num2=31
+let multiply= num1 * num2
+let random = Math.floor(Math.random()*1000 + 1)
+let num3 = 49, num4 = 5
+let mod = num3 % num4
+let max = Math.max(3,6,9,18,15,19,12,10,13,20)
